67b9143e286b26db4a29e14d2e4ec71ef5f25c73
Prapti Yadav <yadav.prapti7777@gmail.com>
2026-02-24 14:31:14 +0530

    Update temperature_monitoring.py

--- temperature_monitoring.py
+++ temperature_monitoring.py
@@ -1,12 +1,20 @@
 import random
 import time  # Used to create delay
 
-# Function to generate random temperature
-def read_temperature():
-    return random.randint(0, 100)
+# Function to generate random temperature within user range
+def read_temperature(min_temp, max_temp):
+    return random.randint(min_temp, max_temp)
 
 # Function to monitor temperature continuously
 def monitor_temperature():
+    # Take user input
+    min_temp = int(input("Enter minimum temperature value: "))
+    max_temp = int(input("Enter maximum temperature value: "))
+
+    # Validate input
+    if min_temp > max_temp:
+        print("Minimum value cannot be greater than maximum value.")
+        return
     while True:  # Infinite loop
         temperature = read_temperature()
         print("Current temperature:", str(temperature) + "°C")
@@ -19,8 +27,9 @@ def monitor_temperature():
         else:
             print("Temperature is within the normal range.")
         
-        print("----------------------")
+    
         time.sleep(2)  # Wait for 2 seconds
 
 # Start monitoring
+
 monitor_temperature()
